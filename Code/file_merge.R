7d279469dfe40147a9289fb1e6ab427bc01c290b
mollycb8 <mollycbell8@gmail.com>
2024-01-08 20:57:09 -0700

    update

--- Code/file_merge.R
+++ Code/file_merge.R
@@ -1,16 +1,16 @@
 # TODO:
-# import all csvs
-# clean data, get rid of unnecessary columns
-# merge multiple files together
-# do API stuff
+
 
 # Import libraries
+
+
 package.list <- c("dplyr",
                   "ggplot2",
                   'tidyverse',
                   'lubridate',
                   "httr",
-                  "jsonlite"
+                  "jsonlite",
+                  "plotly"
 )
 packageLoad <- function(packages){
   for (i in packages) {
@@ -22,13 +22,20 @@ packageLoad <- function(packages){
 }
 packageLoad(package.list)
 
+#Source config code for api key
+
 source('./Code/config.R')
+
+#Read in the letterboxd diary
 
 movies_df <- read.csv("./Data/diary.csv") %>%
   select(c(Name, Rating, Rewatch, Watched.Date)) %>%
-  filter(between(as.Date(Watched.Date), as.Date(paste0(year(Sys.Date()) - 1, "-01-01")), as.Date(paste0(year(Sys.Date()) - 1, "-12-31"))))
+  #Filter to the previous year
+  filter(between(as.Date(Watched.Date), as.Date(paste0(year(Sys.Date()) - 1, "-01-01")), as.Date(paste0(year(Sys.Date()) - 1, "-12-31")))) 
 
-movies_df$Rewatch <- ifelse(movies_df$Rewatch == "Yes", "Yes", "No")
+#Fill in the rewatch column
+movies_df$Rewatch <- ifelse(movies_df$Rewatch == "Yes", "Yes", "No") 
+
 
 
 
@@ -43,15 +50,21 @@ get_movie_details <- function(movie_id) {
   
   movie_credits <- content(response, "parsed")
   return(movie_credits)
+  
+  search_results <- content(response, "parsed")
+ 
 }
+
 
 
 # Create empty DataFrames for cast, crew, and genre
 cast_df <- data.frame(Name = character(0), Cast = character(0))
 crew_df <- data.frame(Name = character(0), Department = character(0), Job = character(0), CrewName = character(0))
-genre_df <- data.frame(Name = character(0), Genre = character(0))
 release_df <- data.frame(Name = character(0), Release.Date = character(0))
+genre_df <- data.frame(Name = character(0), Genre = character(0), stringsAsFactors = FALSE)
 
+
+# Loop through movies_df
 for (i in 1:nrow(movies_df)) {
   movie_name <- movies_df$Name[i]
   search_url <- "https://api.themoviedb.org/3/search/movie"
@@ -65,6 +78,13 @@ for (i in 1:nrow(movies_df)) {
   if (length(search_results$results) > 0) {
     movie_id <- search_results$results[[1]]$id
     cat("Fetching details for movie:", movie_name, "\n")
+    
+    # Fetch movie details directly using /movie/{movie_id} endpoint
+    movie_url <- paste0("https://api.themoviedb.org/3/movie/", movie_id)
+    movie_response <- GET(movie_url, query = list(api_key = api_key))
+    
+    if (status_code(movie_response) == 200) {
+      movie_details <- content(movie_response, "parsed")
     
     # Fetch movie credits
     credits_url <- paste0("https://api.themoviedb.org/3/movie/", movie_id, "/credits")
@@ -81,9 +101,17 @@ for (i in 1:nrow(movies_df)) {
       # Extract release date as character string
       release_date_str <- as.character(search_results$results[[1]]$release_date)
       
-      
       # Update release date in movies_df
       movies_df$Release.Date[movies_df$Name == movie_name] <- release_date_str
+      
+      # Extract runtime information
+      runtime_minutes <- NULL
+      if (!is.null(movie_details$runtime)) {
+        runtime_minutes <- as.numeric(movie_details$runtime)
+      }
+      
+      # Update runtime in minutes in movies_df
+      movies_df$Runtime[movies_df$Name == movie_name] <- runtime_minutes
       
       
       # Extract cast information
@@ -92,7 +120,7 @@ for (i in 1:nrow(movies_df)) {
         cast <- sapply(movie_credits$cast, function(actor) actor$name)
       }
       
-      cast_entry <- data.frame(Name = rep(movie_name, length(cast)), Cast = cast)
+      cast_entry <- data.frame(Name = rep(movie_name, length(cast)), Cast = cast, stringsAsFactors = FALSE)
       cast_df <- bind_rows(cast_df, cast_entry)
       
       # Extract crew information
@@ -103,36 +131,30 @@ for (i in 1:nrow(movies_df)) {
           job <- crew_entry$job
           crew_name <- crew_entry$name
           
-          crew_entry <- data.frame(Name = rep(movie_name, 1), Department = department, Job = job, CrewName = crew_name)
+          crew_entry <- data.frame(Name = rep(movie_name, 1), Department = department, Job = job, CrewName = crew_name, stringsAsFactors = FALSE)
           crew_df <- bind_rows(crew_df, crew_entry)
         }
       }
       
       # Extract genre information
-      genre <- character(0)
-      if (!is.null(movie_credits$genres)) {
-        genres <- sapply(movie_credits$genres, function(genre) genre$name)
-        genre <- genres
+      genres <- character(0)
+      if (!is.null(movie_details$genres)) {
+        genres <- sapply(movie_details$genres, function(genre) genre$name)
       }
       
-      genre_entry <- data.frame(Name = rep(movie_name, length(genre)), Genre = genre)
-      genre_df <- bind_rows(genre_df, genre_entry)
-      
+      # Create a data frame for the current movie and append to genre_df
+      genre_entry <- data.frame(Name = rep(movie_name, length(genres)), Genre = genres, stringsAsFactors = FALSE)
+      genre_df <- dplyr::bind_rows(genre_df, genre_entry)
     } else {
-      cat("Failed to fetch movie credits for:", movie_name, "\n")
-    }
+      print(paste("No genre information found for movie:", movie_name))
+        }
+      }
   }
 }
-
-# Now cast_df, crew_df, genre_df, and release_df contain the added Release.Date column associated with the Name column from movies_df
-
+  
 # Filter crew_df for directors
 dir_df <- crew_df %>% 
   filter(Job == "Director")
-
-
-
-
 
 
 
